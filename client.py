9e5521c24291c6178750ea4687f162acce743ee6
Anders Pitman <tapitman11@gmail.com>
2020-12-18 16:30:04 -0700

    Move GemDriveClient to separate file

---
+++ client.py
@@ -0,0 +1,135 @@
+import os, threading, queue, json, math, shutil
+from urllib import request
+from datetime import datetime
+
+class GemDriveClient():
+
+    def __init__(self, options):
+
+        self.options = options
+        self.job_queue = queue.Queue(maxsize=8)
+
+        for w in range(options['num_workers']):
+            threading.Thread(target=self.downloader, daemon=True).start()
+
+    def run(self):
+        self.traverse(self.options['url'], self.options['out_dir'], None)
+        self.job_queue.join()
+
+    def traverse(self, url, parent_dir, gem_data_in):
+
+        max_depth = self.options['depth']
+        token = self.options['token']
+
+        gem_data = gem_data_in
+
+        if gem_data is None:
+            gem_url = url + 'gemdrive/meta.json?depth=' + str(max_depth)
+
+            if token is not None:
+                gem_url += '&access_token=' + token
+
+            res = request.urlopen(gem_url)
+            body = res.read()
+            gem_data = json.loads(body)
+
+        if not os.path.isdir(parent_dir):
+            print("Create", parent_dir)
+            if self.options['dry_run']:
+                # Early return for dry run because attempts to compare child
+                # directories which don't exist will cause exceptions.
+                return
+            else:
+                os.mkdir(parent_dir)
+
+        for child_name in gem_data['children']:
+            child = gem_data['children'][child_name]
+            child_url = url + child_name
+            child_path = os.path.join(parent_dir, child_name)
+            is_dir = child_url.endswith('/')
+            if is_dir:
+                child_gem_data = child
+                if 'children' not in child:
+                    child_gem_data = None
+
+                self.traverse(child_url, child_path, child_gem_data)
+            else:
+                self.job_queue.put((child_url, parent_dir, child))
+
+        if self.options['delete']:
+            with os.scandir(parent_dir) as it:
+                for entry in it:
+                    name = entry.name
+                    if entry.is_dir():
+                        name += '/'
+
+                    if name not in gem_data['children']:
+                        item_path = os.path.join(parent_dir, name)
+                        print("Delete", item_path)
+
+                        if not self.options['dry_run']:
+                            if entry.is_dir():
+                                shutil.rmtree(item_path)
+                            else:
+                                os.remove(item_path)
+
+
+    def downloader(self):
+        while True:
+            url, parent_dir, gem_data = self.job_queue.get()
+
+            self.handle_file(url, parent_dir, gem_data)
+
+            self.job_queue.task_done()
+
+
+    def handle_file(self, url, parent_dir, gem_data):
+
+        if self.options['verbose']:
+            print(url)
+
+        token = self.options['token']
+
+        name = os.path.basename(url)
+        path = os.path.join(parent_dir, name)
+
+        try:
+            stat = os.stat(path)
+        except:
+            stat = None
+
+        size = 0
+        mod_time = ''
+        if stat:
+            size = stat.st_size
+            mod_time = datetime.utcfromtimestamp(stat.st_mtime).replace(microsecond=0).isoformat() + 'Z'
+
+        utc_dt = datetime.strptime(gem_data['modTime'], '%Y-%m-%dT%H:%M:%SZ')
+        mtime = math.floor((utc_dt - datetime(1970, 1, 1)).total_seconds())
+
+        needs_update = size != gem_data['size'] or mod_time != gem_data['modTime']
+
+        if needs_update:
+            print("Sync", url)
+
+            if not self.options['dry_run']:
+                file_url = url
+
+                if token is not None:
+                    file_url += '?access_token=' + token
+
+                res = request.urlopen(file_url)
+                with open(path, 'wb') as f:
+                    while True:
+                        chunk = res.read(4096)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                stat = os.stat(path)
+
+                if stat.st_size != gem_data['size']:
+                    print("Sizes don't match", url)
+
+                os.utime(path, (stat.st_atime, mtime))
+
+
